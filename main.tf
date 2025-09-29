074daf87f1f6a1eaabe20818febed25a50295ef9
Gopendra <gopendra971@gmail.com>
2025-09-29 11:20:45 +0000

    Remove main.tf

--- main.tf
+++
@@ -1,5 +0,0 @@
-module "ec2_instance" {
-  source = "git::https://github.com/mahroot/terraform_module_repo.git?ref=main"
-  ami_value = "ami-02d26659fd82cf299"
-  instance_type_value = "t2.micro"
-}
